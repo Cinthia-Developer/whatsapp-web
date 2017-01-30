0137b6c39285833b11ae7b08687082fc84e37be7
Cinthia <cinthiamashiel474@gmail.com>
2017-01-30 09:24:42 -0500

    rectificacion de variables

--- js/main.js
+++ js/main.js
@@ -1,41 +1,36 @@
     var mensaje = document.getElementById("mensajes");
     var chat = document.getElementById("chat");
 	mensaje.addEventListener('keyup', validarTecla);
-
+// para que la tecla (enter) funcione:
 function validarTecla(evt){
     if(evt.keyCode == 13 && mensaje.value.length != 0){
         agregarMensaje();
     }
 }
-
+// funcion para agregar un mensaje al chat con hora de envio:
 function agregarMensaje(){
-    var horario = new Date();
-	var hora = horario.getHours();
-	var minuto = horario.getMinutes();
+    var nuevoMensaje = mensaje.value;
+// para agregar la hora de envio al chat:
+    var fecha = new Date();
+	var hora = fecha.getHours();
+	var min = fecha.getMinutes();
+// para agregar mensaje al chat:    
+	var div1 = document.createElement("div");
+	div1.className = "w-message w-message-out";
+    chat.appendChild(div1);
     
-	var texto = mensaje.value;
+	var div2 = document.createElement("div");
+	div2.className = "w-message-text";
+    div1.appendChild(div2);
     
-	var cuadro = document.createElement('div');
-	cuadro.className = "w-message w-message-out";
-    chat.appendChild(cuadro);
+	var parrafo = document.createElement("p");
+    div2.appendChild(parrafo);
+    parrafo.innerHTML = nuevoMensaje;
     
-	var cuadro2 = document.createElement('div');
-	cuadro2.className = "w-message-text";
-    cuadro.appendChild(cuadro2);
-    
-	var parrafo = document.createElement('p');
-    cuadro2.appendChild(parrafo);
-    parrafo.innerHTML = texto;
-    
-	var time = document.createElement('div');
+	var time = document.createElement("div");
 	time.className = "time";
-    cuadro2.appendChild(time);
-    time.innerHTML = hora + ":" + minuto;
-	
-    if(minuto<10){
-		minuto='0'+minuto;
-	}
+    div2.appendChild(time);
+    time.innerHTML = hora + ":" + min;
+// para limpiar el input una vez enviado el mensaje:	
 	mensaje.value="";
-    mensaje.focus();
-    
 }
