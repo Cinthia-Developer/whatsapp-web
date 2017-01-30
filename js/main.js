43f6355650ef1946ea2bb9819161ab55afc2773e
Cinthia <cinthiamashiel474@gmail.com>
2017-01-30 09:12:28 -0500

    envia mensajes con la hora de envio

--- js/main.js
+++ js/main.js
@@ -1,35 +1,41 @@
-var mensaje = document.getElementById("mensajes");
-var chat = document.getElementById("chat");
-// para el valor del input.    
-    var mensajeTotal = mensaje.value;
+    var mensaje = document.getElementById("mensajes");
+    var chat = document.getElementById("chat");
+	mensaje.addEventListener('keyup', validarTecla);
 
-function validar(event) {
-  mensajeTotal = (document.all) ? event.keyCode : event.which;
-  if (mensajeTotal==13) alert ('Has pulsado enter');
+function validarTecla(evt){
+    if(evt.keyCode == 13 && mensaje.value.length != 0){
+        agregarMensaje();
     }
-// funcion que agrega mensaje
-function agregar(){
-// para la hora
-    var fecha = new Date();
-    var hora = fecha.getHours()+":"+fecha.getMinutes();
-// para crear nuevos mensajes.
-    var div1 = document.createElement("div");
-    div1.className = "w-message w-message-out";
-    chat.appendChild(div1);
-    
-    var div2 = document.createElement("div");
-    div2.className = "w-message-text";
-    div1.appendChild(div2);
-    
-    var p = document.createElement("P");
-    div2.appendChild(p);
-    p.appendChild(document.createTextNode(mensajeTotal));
-    
-    var time = document.createElement("div");
-    time.ClassName = "time";
-    div2.appendChild(time);
-    time.innerHTML=hora;
 }
 
-mensaje.addEventListener("keyup", agregar);
-
+function agregarMensaje(){
+    var horario = new Date();
+	var hora = horario.getHours();
+	var minuto = horario.getMinutes();
+    
+	var texto = mensaje.value;
+    
+	var cuadro = document.createElement('div');
+	cuadro.className = "w-message w-message-out";
+    chat.appendChild(cuadro);
+    
+	var cuadro2 = document.createElement('div');
+	cuadro2.className = "w-message-text";
+    cuadro.appendChild(cuadro2);
+    
+	var parrafo = document.createElement('p');
+    cuadro2.appendChild(parrafo);
+    parrafo.innerHTML = texto;
+    
+	var time = document.createElement('div');
+	time.className = "time";
+    cuadro2.appendChild(time);
+    time.innerHTML = hora + ":" + minuto;
+	
+    if(minuto<10){
+		minuto='0'+minuto;
+	}
+	mensaje.value="";
+    mensaje.focus();
+    
+}
